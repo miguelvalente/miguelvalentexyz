c0cb615a056de14a857ecd7762800bbd03b24756
miguelvalente <miguelvalente@protonmail.com>
2023-03-18 14:44:50 +0100

    nothing at all

--- themes/hba-revamped/assets/js/themeSwitchnMenu.js
+++ themes/hba-revamped/assets/js/themeSwitchnMenu.js
@@ -19,14 +19,25 @@
       toggleTheme(localStorage.getItem("theme"))
     );
   
-    // Blur the content when the menu is open
+    // Blur the content
+    // and remove the blur when the menu is closed
+    // or when the user clicks outside the menu
+    // the menu is a ul element with the class "triger"
     const cbox = document.getElementById("menu-trigger");
-  
     cbox.addEventListener("change", function () {
       const area = document.querySelector(".wrapper");
+      console.log('blur');
       this.checked
         ? area.classList.add("blurry")
         : area.classList.remove("blurry");
     });
+
+    const cbox_menu = document.getElementsById("over");
+    console.log(cbox_menu);
+    cbox_menu.addEventListener('change', function() {
+      console.log('mouse');
+      isMouseDown = true;
+    });
+
   })();
   
